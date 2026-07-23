eed9788715b523d4ca271ffe45cea2a4da961734
KATHA SNEHITHA <122622282+KathaSnehitha@users.noreply.github.com>
2026-07-23 22:51:51 +0530

    refactor: simplify home hero

--- frontend/src/features/home/components/HeroSection.jsx
+++ frontend/src/features/home/components/HeroSection.jsx
@@ -1,11 +1,7 @@
 import { Container } from "@/components/layout/Container";
-import { TwoColumnLayout } from "@/components/layout/TwoColumnLayout";
 import { ScrollIndicator } from "@/components/ui/scroll-indicator";
 import { HeroBackgroundVideo } from "@/features/home/components/HeroBackgroundVideo";
 import { HeroContent } from "@/features/home/components/HeroContent";
-import { HeroFeaturesGrid } from "@/features/home/components/HeroFeaturesGrid";
-import { HeroAssociationSection } from "@/features/home/components/HeroAssociationSection";
-import { HeroUGCCard } from "@/features/home/components/HeroUGCCard";
 import { useHomeContent } from "@/features/home/hooks/useHomeContent";
 
 export function HeroSection() {
@@ -20,16 +16,7 @@ export function HeroSection() {
       <HeroBackgroundVideo src={hero.backgroundMedia?.src} poster={hero.backgroundMedia?.poster} />
 
       <Container className="w-full pt-28 pb-16 lg:pt-24 lg:pb-20">
-        <TwoColumnLayout
-          left={<HeroContent hero={hero} />}
-          right={
-            <div className="flex flex-col gap-8 lg:justify-center">
-              <HeroFeaturesGrid features={hero.features} />
-              <HeroAssociationSection associations={hero.associations} />
-              <HeroUGCCard ugc={hero.ugc} />
-            </div>
-          }
-        />
+        <HeroContent hero={hero} />
       </Container>
 
       <div className="flex justify-center pb-8 lg:absolute lg:inset-x-0 lg:bottom-8 lg:pb-0">
