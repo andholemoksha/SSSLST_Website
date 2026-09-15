5ba5629499bdcba531d2fc9478b52bb9c823f32a
Andhole Moksha <andhole_moksha@epam.com>
2026-09-16 00:22:36 +0530

    base.py changes

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -31,7 +31,7 @@ SECRET_KEY = config(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '[::1]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '[::1]','ssslst-website-1.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
